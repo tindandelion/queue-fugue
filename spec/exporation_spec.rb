256c380d76fd64998b81d93b27aa966c72c27ee1
Sergey Moshnikov <s.moshnikov@gmail.com>
2013-03-30 08:57:30 +0400

    A spec for sending a message

--- spec/exporation_spec.rb
+++ spec/exporation_spec.rb
@@ -2,11 +2,25 @@ require 'activemq-all-5.8.0.jar'
 require 'pry'
 
 describe "Exploring ActiveMQ interface" do
-  it "connects to an instance of ActiveMQ" do
+
+  let(:connection) do
     connection_factory = org.apache.activemq.ActiveMQConnectionFactory.new('tcp://localhost:61616')
-    connection = connection_factory.create_connection
+    connection_factory.create_connection
+  end
+
+  let(:session) { connection.create_session(false, javax.jms.Session::AUTO_ACKNOWLEDGE) }
+  
+  after :each do
+    session.close
+    connection.close
+  end
+  
+  it "sends a message to the queue" do
+    queue = session.create_queue("TEST")
+    producer = session.create_producer(queue)
+    producer.set_delivery_mode(javax.jms.DeliveryMode::NON_PERSISTENT)
     
-    binding.pry
-    
+    message = session.create_text_message('Hello world!')
+    producer.send(message)
   end
 end
